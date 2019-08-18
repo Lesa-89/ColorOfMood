cb231f834dc5a8260acb977571c6d8ba314d9267
Larichev Sergey <lesa-89@mail.ru>
2019-08-19 01:19:54 +0300

    Дизайн 2.1

--- ColorOfMood/Controller/ResultController.swift
+++ ColorOfMood/Controller/ResultController.swift
@@ -22,6 +22,7 @@ class ResultController: UIViewController {
     @IBOutlet var resultTextView: UITextView!
     
     @IBOutlet var doneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         resultLabel.text = "Цвет вашего настроения - \(viewColor.rawValue)"
